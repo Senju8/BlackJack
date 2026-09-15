b1b7b1fddf6e0b35cff776c35d00919bf1e743f9
unknown <264014@lab-ocs.local>
2026-09-15 15:38:51 +0900

    Updated bet

--- Assets/Scripts/System/BetPhase.cs
+++ Assets/Scripts/System/BetPhase.cs
@@ -39,7 +39,7 @@ namespace System
                     // ベットを確定したときにショップフェーズへ遷移する
                     betButtoms.OnBetConfirmed += betAmount =>
                     {
-                        this.gameManager.Call("shop");
+                        this.gameManager.Call("blackjack");
                     };
                 });
 
